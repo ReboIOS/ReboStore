64f8846ed483a0e8cebd6aba079b837b4ae68762
ReboIOS <73317171+ReboIOS@users.noreply.github.com>
2022-04-17 01:09:31 +0300

    Update 8ball.php

--- pages/8ball.php
+++ pages/8ball.php
@@ -33,7 +33,7 @@
                   <div class="item-title">
                     8 BallPool Hack
                   </div><a href=
-                  "itms-services://?action=download-manifest&url=https://www.rebo-store.com/install/8ball.plist"
+                  "itms-services://?action=download-manifest&url=https://www.rebo-store.com/install/8ballpool.plist"
                   class=
                   "button button-round button-fill button-raised color-green external">Get
                   <i class=
