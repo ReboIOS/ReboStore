434d333b0936c1c8badad884bc77779e3825a6fd
ReboIOS <73317171+ReboIOS@users.noreply.github.com>
2022-04-16 20:20:17 +0300

    Add files via upload

---
+++ x/js/routes.js
@@ -0,0 +1,63 @@
+routes = [
+  {
+    path: '/',
+    url: '/store/',
+  },
+  {
+    path: '/:id/',
+    url: '/section/{{id}}',
+  },
+  {
+    path: '/appleid',
+    url: './pages/appleid.php',
+  },
+  {
+    path: '/odyssey',
+    url: './pages/odyssey.php',
+  },
+  {
+    path: '/unc0ver',
+    url: './pages/unc0ver.php',
+  },
+  {
+    path: '/videostar',
+    url: './pages/videostar.php',
+  },
+  {
+    path: '/pubg',
+    url: './pages/pubg.php',
+  },
+  {
+    path: '/8ball',
+    url: './pages/8ball.php',
+  },
+  {
+    path: '/appcake',
+    url: './pages/appcake.php',
+  },
+  {
+    path: '/minecraft',
+    url: './pages/minecraft.php',
+  },
+  {
+    path: '/videofx',
+    url: './pages/videofx.php',
+  },
+  {
+    path: '/picsart',
+    url: './pages/picsart.php',
+  },
+  {
+    path: '/install',
+    url: './pages/install.php',
+  },
+  {
+    path: '/theming',
+    componentUrl: './pages/theme.php',
+  },
+  // Default route (404 page). MUST BE THE LAST
+  {
+    path: '(.*)',
+    url: './pages/404.php',
+  },
+];
