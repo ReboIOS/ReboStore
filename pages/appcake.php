019b99a323453984c587917b078d49d6d2a2d368
ReboIOS <73317171+ReboIOS@users.noreply.github.com>
2022-04-25 05:04:35 +0300

    Update appcake.php

--- pages/appcake.php
+++ pages/appcake.php
@@ -32,8 +32,7 @@
                 <div class="item-title-row">
                   <div class="item-title">
                     AppCake
-                  </div><a href=
-                  "itms-services://?action=download-manifest&url=https://www.rebo-store.com/install/appcake.plist"
+                  </div><a href="itms-services://?action=download-manifest&url=https://www.rebo-store.com/install/appcake.plist"
                   class=
                   "button button-round button-fill button-raised color-green external">Get
                   <i class=
