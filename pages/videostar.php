bd4043b532f72cd938a295eef038508e9ea9ec6b
ReboIOS <73317171+ReboIOS@users.noreply.github.com>
2022-04-25 05:12:21 +0300

    Update videostar.php

--- pages/videostar.php
+++ pages/videostar.php
@@ -32,8 +32,7 @@
                 <div class="item-title-row">
                   <div class="item-title">
                     VideoStar++
-                  </div><a href=
-                  "itms-services://?action=download-manifest&url=https://www.rebo-store.com/install/videostar.plist"
+                  </div><a href="itms-services://?action=download-manifest&url=https://www.rebo-store.com/install/videostar.plist"
                   class=
                   "button button-round button-fill button-raised color-green external">Get
                   <i class=
