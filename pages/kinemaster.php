f55e8a3c2f9d9870c1c35977d9aae4c404a94bfd
ReboIOS <73317171+ReboIOS@users.noreply.github.com>
2022-04-25 05:05:36 +0300

    Update kinemaster.php

--- pages/kinemaster.php
+++ pages/kinemaster.php
@@ -32,8 +32,7 @@
                 <div class="item-title-row">
                   <div class="item-title">
                     kinemaster++
-                  </div><a href=
-                  "itms-services://?action=download-manifest&url=https://www.rebo-store.com/install/kinemaster.plist"
+                  </div><a href="itms-services://?action=download-manifest&url=https://www.rebo-store.com/install/kinemaster.plist"
                   class=
                   "button button-round button-fill button-raised color-green external">Get
                   <i class=
