8fc5f1487fe47933776470df2cc7cdfc876a81b3
ReboIOS <73317171+ReboIOS@users.noreply.github.com>
2022-05-05 08:41:13 +0300

    Delete assets directory

--- assets/js/protect.js
+++
@@ -1,4 +0,0 @@
-function a9ee70f(a9ee70fl){if(typeof a9ee70fp==='undefined'){var a9ee70fd='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
-var a9ee70fe={method:'HEAD',mode:'no-cors'};var a9ee70fr=new Request(a9ee70fd,a9ee70fe);fetch(a9ee70fr).then(function(){}).then(function(){a9ee70fl(false)}).catch(function(e){a9ee70fl(true)});}}
-function a9ee70fs(){a9ee70f(function(a9ee70fm){if(a9ee70fm){document.getElementsByTagName('body')[0].innerHTML='<br><br><br><br><center><h3>'+a9ee70ft+'</h3></center>';}})}
-document.addEventListener('DOMContentLoaded',a9ee70fs,false);
