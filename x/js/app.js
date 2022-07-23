2829a8c176cdb1dd84f868d986c97ffa3cb93d33
ReboIOS <73317171+ReboIOS@users.noreply.github.com>
2022-07-24 00:52:46 +0300

    Update app.js
    Signed-off-by: ReboIOS <73317171+ReboIOS@users.noreply.github.com>

--- x/js/app.js
+++ x/js/app.js
@@ -44,3 +44,15 @@ var settingsView = app.views.create('#view-iphone', {
 var settingsView = app.views.create('#view-android', {
   url: '/android/'
 });
+
+const popup = document.querySelector('.popup');
+const x = document.querySelector('.popup-content h1')
+
+window.addEventListener('load', () => {
+   popup.classList.add('showPopup');
+   popup.childNodes[1].classList.add('showPopup');
+     })
+     x.addEventListener('click', () => {
+         popup.classList.remove('showPopup');
+         popup.childNodes[1].classList.remove('showPopup');
+     })
