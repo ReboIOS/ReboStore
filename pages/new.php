02c7057db12498459083324365ae194ffca13314
ReboIOS <73317171+ReboIOS@users.noreply.github.com>
2022-07-25 20:15:04 +0300

    Add files via upload

--- pages/new.php
+++ pages/new.php
@@ -23,6 +23,27 @@
     <div class="page-content">
       <div class="list media-list inset elevation-12">
         <ul>
+                                <li class="media-item">
+                                    <a href="/esign" class="item-link">
+                                        <div class="item-content">
+                                            <div class="item-media">
+                                                <img src="img/esign.png" class="appiconx elevation-5 animated zoomIn"></div>
+                                            <div class="item-inner">
+                                                <div class="item-title-row">
+                                                    <div class="item-title">ESign</div>
+                                                </div>
+                                                <div class="chip">
+                                                    <div class="chip-media bg-color-blue"><i class="icon f7-icons">gear</i></div>
+                                                    <div class="chip-label">153.0</div>
+                                                </div>
+                                                <div class="chip">
+                                                    <div class="chip-media bg-color-blue"><i class="icon f7-icons">download</i></div>
+                                                    <div class="chip-label">110 MB</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </li>
           <li class="media-item">
                                 <li class="media-item">
                                     <a href="/ppsspp" class="item-link">
